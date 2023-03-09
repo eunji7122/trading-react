da3b7f6e4fb47a8f2df44aea68870ef926ba85e8
eunji <lej7122@gmail.com>
2023-03-10 01:14:57 +0900

    차트 헤더 실시간 조회 추가

--- components/trading/TradingChartHeader.tsx
+++ components/trading/TradingChartHeader.tsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TradingPair } from "../../model/trading-pair";
+import { useAppSelector } from "../../app/store";
 
 interface Props {
   tradingPair: TradingPair;
 }
 
 export default function TradingChartHeader({ tradingPair }: Props) {
+  const [useStateTradingPair, setUseStateTradingPair] = useState<TradingPair>();
+
+  const findTradingPair = useAppSelector((state) =>
+    state.tradingPair.tradingPairs.find((x) => x.id == tradingPair.id)
+  );
+
+  useEffect(() => {
+    setUseStateTradingPair(tradingPair);
+  }, [tradingPair]);
+
+  useEffect(() => {
+    setUseStateTradingPair(findTradingPair);
+  }, [findTradingPair]);
+
   return (
     <div className="bg-white m-2">
       <div className="flex flex-row justify-between border-b">
         <p className="font-bold text-2xl text-gray-800 py-1 px-2 m-1">
-          {tradingPair?.baseAsset.name}
+          {useStateTradingPair?.baseAsset.name}
         </p>
       </div>
       <div className="flex">
         <div className="w-1/2 py-1 px-2 m-1">
-          <p className="font-bold text-3xl">{tradingPair?.lastPrice}</p>
-          <p className="text-sm">전일대비 {tradingPair.changeRate}%</p>
+          <p className="font-bold text-3xl">{useStateTradingPair?.lastPrice}</p>
+          <p className="text-sm">전일대비 {useStateTradingPair?.changeRate}%</p>
         </div>
         <div className="w-1/2 py-5 flex text-sm">
           <div className="flex w-1/2 justify-between px-5">
@@ -26,10 +41,10 @@ export default function TradingChartHeader({ tradingPair }: Props) {
             </div>
             <div className="h-1/2">
               <p className="text-red-600 font-bold">
-                {tradingPair.highestPrice}
+                {useStateTradingPair?.highestPrice}
               </p>
               <p className="text-blue-600 font-bold">
-                {tradingPair.lowestPrice}
+                {useStateTradingPair?.lowestPrice}
               </p>
             </div>
           </div>
@@ -39,8 +54,8 @@ export default function TradingChartHeader({ tradingPair }: Props) {
               <p>거래대금</p>
             </div>
             <div className="h-1/2">
-              <p>{tradingPair.tradingAmount}</p>
-              <p>{tradingPair.tradingValue}</p>
+              <p>{useStateTradingPair?.tradingAmount}</p>
+              <p>{useStateTradingPair?.tradingValue}</p>
             </div>
           </div>
         </div>
